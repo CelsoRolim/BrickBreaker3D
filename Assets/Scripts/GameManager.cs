9f8c1137853cb36979a5d9655ae087cb7adec4f5
Celfi <Celfi>
2016-08-03 20:42:46 -0400

    Title: Added GameManager Developer: Celso

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public static GameManager instance = null;
+
+	void Awake()
+	{
+		if (instance == null) {
+			instance = this;
+		} 
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+		}
 	}
 }
